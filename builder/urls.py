278b80b04e0e73bda3d0b1745974c1e75976f7de
Joaquín Begué <joaquinbegue1204@gmail.com>
2022-10-21 17:35:46 -0300

    Proyect paused.

--- builder/urls.py
+++ builder/urls.py
@@ -1,9 +1,14 @@
-from django import urls
+from django.urls import path
 
 from . import views
 
 app_name = 'builder'
 
 urlpatterns = [
-    
+    # Index page / Brand choosing page.
+    path('', views.index, name='index'),
+    # CPU choosing page.
+    path('cpu/<order>', views.cpu, name='cpu'),
+    # Motherboard choosing page.
+    path('motherboard/<order>/', views.motherboard, name='motherboard'),
 ]
--- builder/views.py
+++ builder/views.py
@@ -1,3 +1,66 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+from .models import Component
+
+
+def index(request):
+    """The starting page. The user chooses the cpu's brand."""
+    order = url_str() # An order with the components the user chooses.
+    context = {'order': order}
+    try:
+        brand = request.POST['brand']
+    except KeyError:
+        return render(request, 'builder/index.html', context)
+    else:
+        order = url_str(order, f"brand:{brand}-")
+        return HttpResponseRedirect(reverse('builder:cpu',
+            args=(order,)))
+
+
+def cpu(request, order):
+    """The user chooses the CPU."""
+    splitted_order = split_order(order)
+    order = [tuple(order.split(":"))]
+    available_comps = Component.objects.filter(c_type='CPU').filter(
+        model__icontains=order[0][1])
+    order = order_raw
+    context = {'components': available_comps, 'order': order}
+
+    try:
+        chosen_cpu = available_comps.get(pk=request.POST['cpu'])
+        chosen_cpu = request.POST['cpu']
+    except  (KeyError, Component.DoesNotExist):
+        return render(request, 'builder/cpu.html', context)
+    else:
+        order += "-cpu:" + chosen_cpu
+        return HttpResponseRedirect(reverse('builder:motherboard',
+            args=(order,)))
+   
+
+def motherboard(request, order):
+    order_raw = order
+    order_split = order.split("-")
+    order = []
+    for o in order_split:
+        order.append(tuple(o.split(":")))
+    context = {'order': order}
+    return render(request, 'builder/motherboard.html', context)
+
+
+def url_str(order=None, string=None):
+    if order and string:
+        order += string
+        return order
+    else:
+        return 'order'
+
+
+def split_order(order):
+    fields = order.split("-")
+    splitted_order = []
+    for field in fields:
+        splitted_order.append(tuple(field.split(":")))
+    return splitted_order
+    
